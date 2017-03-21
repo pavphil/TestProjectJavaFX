4acb06ea2ec8fc84ebf15721a8ba6c60d891be4d
Philip <phil955@yandex.ru>
2017-03-22 00:19:18 +0300

    Merge branch 'newlocalbranch'
    # Conflicts:
    #	main/Main.java

--- main/Main.java
+++ main/Main.java
@@ -68,8 +68,7 @@ public class Main extends Application {
             grid1.add(tn, 0, 0);
             grid1.add(closeButton, 1, 0);
             //Thread.setDefaultUncaughtExceptionHandler(h);
-        //non thread
-        //newlocalbranch
+        //changes in localbranch
             bp.setOnKeyPressed(new EventHandler<KeyEvent>() {
                 @Override
                 public void handle(KeyEvent event) {
