0c82eba95f3c49463c99ac645f47207f2a5f9c51
Philip <phil955@yandex.ru>
2017-03-21 23:57:10 +0300

    .

--- main/Main.java
+++ main/Main.java
@@ -67,7 +67,8 @@ public class Main extends Application {
             });
             grid1.add(tn, 0, 0);
             grid1.add(closeButton, 1, 0);
-            Thread.setDefaultUncaughtExceptionHandler(h);
+            //Thread.setDefaultUncaughtExceptionHandler(h);
+        //вввв
             bp.setOnKeyPressed(new EventHandler<KeyEvent>() {
                 @Override
                 public void handle(KeyEvent event) {
